cf98a481227a843a8f9a73b05ab2a8d31e373afb
James Min <jmin499@gmail.com>
2019-11-01 21:11:37 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song
+  
+  attr_reader :name
+  attr_accessor :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+  
+end
